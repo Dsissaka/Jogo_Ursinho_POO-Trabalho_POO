0bdd7dc3b456d1738cdd61542ba356bf7329797e
Issaka Diaw <dsissaka@gmail.com>
2025-06-02 14:09:20 -0300

    Update projetil.py

--- Biblioteca/projetil.py
+++ Biblioteca/projetil.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Projetil:
-    def __init__(self, velocidade_projetil, pos_x_projetil, pos_y_projetil, direcao, ativo, largura_proj, altura_proj):
+    def __init__(self, velocidade_projetil, pos_x_projetil, pos_y_projetil, direcao, ativo, largura_proj, altura_proj, sprite):
         self.velocidade_projetil = velocidade_projetil
         self.pos_x_projetil = pos_x_projetil
         self.pos_y_projetil = pos_y_projetil
@@ -9,6 +9,7 @@ class Projetil:
         self.ativo = ativo 
         self.altura_proj = altura_proj
         self.largura_proj = largura_proj
+        self.sprite =sprite
 
     def desativar(self):
         #função para causar a exclusão da bala após ela sair do mapa ou após acertar o jogador
@@ -23,15 +24,17 @@ class Projetil:
         hitbox_outro = pygame.Rect(outro.pos_x, outro.pos_y, outro.tam_x, outro.tam_y)
         return hitbox_projetil.colliderect(hitbox_outro)
         
-    def atualizar(self, outro):
-                    self.mover()
-                    if self.verifica_colisao(outro):
-                            outro.vida = outro.vida - 1 #caso atingido, O player perde uma vida
-                            self.desativar() #exclui a bala 
-                            return
-                    if self.pos_x_projetil < 0 or self.pos_x_projetil > largura_tela: #caso saia do mapa
-                        self.desativar()
+    def atualizar(self, outro,largura_tela):
+        self.mover()
+        if self.verifica_colisao(outro):
+            outro.vida = outro.vida - 1 #caso atingido, O player perde uma vida
+            self.desativar() #exclui a bala 
+            return
+        if self.pos_x_projetil < 0 or self.pos_x_projetil > largura_tela: #caso saia do mapa
+            self.desativar()
 
-                #(decidir se talvez definir um timeout para chamada da função "atualizar" ou faze quando "verificar_colisão" for true)
+
+            
+#(decidir se talvez definir um timeout para chamada da função "atualizar" ou faze quando "verificar_colisão" for true)
 
         
