d0043e122533a6e058e2e89cbd1f3eee4a87b504
Issaka Diaw <dsissaka@gmail.com>
2025-06-14 22:58:05 -0300

    Update JOGOFINAL.py

--- JOGOFINAL.py
+++ JOGOFINAL.py
@@ -3,18 +3,37 @@ import pygame
 from Biblioteca import Projetil as Pj # IMPORTA A CLASSE PROJETIL
 from Biblioteca import Animacao as Am #Importa a classe animacao
 from Biblioteca import saveLoadManager as Slm
+from Biblioteca import Prepara_jogo as Pg
 
 ALTURA_TELA = 600
 COMPRIMENTO_TELA = 800
 INTERVALO_DISPARO = 3000 # 1.5 segundos
 
-def verifica_colisao(self, outro):
-        hitbox_self = pygame.Rect(self._pos_x, self._pos_y, self._tam_x, self._tam_y)
-        hitbox_outro = pygame.Rect(outro._pos_x, outro._pos_y, outro._tam_x, outro._tam_y)
-        return hitbox_self.colliderect(hitbox_outro)
 
-class Personagens():
-    def __init__(self, id_character,  name_character, pos_x, pos_y, vida, tam_x, tam_y, sprite):
+class Plataform(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h): #dimensões e posição
+        super().__init__()
+        self.image = pygame.Surface([w,h]) #criando a plataforma
+        self.image.fill([139, 69, 19])# as cores dela
+        #criando o hitbox dela:
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Honey(pygame.sprite.Sprite):
+    def __init__(self, x, y, sprite):
+        super().__init__()
+        self.animacao = Am.Animacao(sprite, "honey_sprites", 100)
+        self.image = self.animacao.pega_sprite_atual()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+    def update(self):
+        self.image = self.animacao.pega_sprite_atual()
+
+class Personagens(pygame.sprite.Sprite, ABC):
+    def __init__(self, id_character,  name_character,  sprite, pos_x, pos_y, tam_x, tam_y):
         self._id_character= id_character #id de identificação do personagem
         self._name_character= name_character #nome do personagem
         self._pos_x = pos_x #posicao no eixo X do personagem em relação ao mapa
@@ -36,11 +55,11 @@ class Personagens():
         # e a variavel tipo para identificar o tipo da animação
         pass
 
-    @abstractmethod
     def verifica_colisao(self, outro):
-        #verifica se a posição do objeto self coincide com a do objeto outro. Assim permitindo intereção do POOH com NPC, além do ataque 
-        # e hit de projeteis inimigos. Utiliza a variavel "outro" para identificar como calcular a colisão
-        pass
+        hitbox_self = pygame.Rect(self._pos_x, self._pos_y, self._tam_x, self._tam_y)
+        hitbox_outro = pygame.Rect(outro._pos_x, outro._pos_y, outro._tam_x, outro._tam_y)
+        return hitbox_self.colliderect(hitbox_outro)
+
 
     @abstractmethod
     def movimento(self):
@@ -50,22 +69,22 @@ class Personagens():
         pass
 
 class Npc(Personagens):
-    def __init__(self,  name_character, id_character, sprite, pos_x, pos_y, vida, tam_x, tam_y,):
-        super().__init__(  name_character, id_character, sprite, pos_x, pos_y, vida, tam_x, tam_y)
+    def __init__(self,  id_character, name_character, sprite, pos_x, pos_y, tam_x, tam_y,):
+        super().__init__( id_character,  name_character, sprite, pos_x, pos_y, tam_x, tam_y)
         self.animacao = Am.Animacao(sprite,"npc_idle_sprites", 100)
 
-    def comentario(self):
-        text_box = "Cuidado com as abelhas" #frase dita pelo npc ao haver colisão com o personagem principal
-        print(text_box) #ainda definir como fazer isto aparecer na tela e não no terminal
+    def comentario(self, surface):
+        fonte = pygame.font.Font(None, 24)
+        texto = fonte.render("Cuidado com as abelhas!", True, (255, 255, 255))
+        surface.blit(texto, (self._pos_x, self._pos_y - 30))
 
 
     def fazer_animacao(self, tipo):
         self.animacao.definir_estado(tipo)
 
-    
-class Player(Personagens, pygame.sprite.Sprite):
-    def __init__ (self, name_character, id_character, sprite, pos_x, pos_y, tam_x, tam_y, speed_x, speed_y):
-        super().__init__(name_character, id_character,  sprite, pos_x, pos_y, tam_x, tam_y,)
+class Player(Personagens):
+    def __init__ (self, id_character, name_character,  sprite, pos_x, pos_y, tam_x, tam_y, speed_x, speed_y):
+        super().__init__(id_character, name_character,   sprite, pos_x, pos_y, tam_x, tam_y,)
         #criando imagem do player
         self.animacao = Am.Animacao(sprite, "pooh_idle_sprites", 100)
         self.image = self.animacao.pega_sprite_atual()
@@ -88,7 +107,7 @@ class Player(Personagens, pygame.sprite.Sprite):
     def fazer_animacao(self, tipo):
         self.animacao.definir_estado(tipo)
 
-    def movimento(self, plataforms_group):
+    def update(self, plataforms_group):
         animacao = "pooh_idle_sprites"
         tecla = pygame.key.get_pressed()
 
@@ -113,6 +132,9 @@ class Player(Personagens, pygame.sprite.Sprite):
             self._no_chao = True 
 
         self.fazer_animacao(animacao)
+        self.rect.x = self._pos_x
+        self.rect.y = self._pos_y
+
 
         # Detecção com Plataformas:
         # Verifique se o jogador está caindo (velocidade positiva)
@@ -133,10 +155,9 @@ class Player(Personagens, pygame.sprite.Sprite):
                 self.invincible = False
                 self.image.set_alpha(255) # garante que o player volte normal 
 
-
 class Inimigo(Personagens):
-    def __init__(self, name_character,  id_character, sprite, pos_x, pos_y, vida, tam_x, tam_y):
-        super().__init__( name_character, id_character, sprite, pos_x, pos_y, vida, tam_x, tam_y,)
+    def __init__(self, id_character, name_character,  sprite, pos_x, pos_y, vida, tam_x, tam_y):
+        super().__init__( id_character, name_character, sprite, pos_x, pos_y, vida, tam_x, tam_y,)
         self.inimigo_ativo= True
         self.lim_sup = 400
         self.lim_inf = 100
@@ -158,13 +179,13 @@ class Inimigo(Personagens):
         
         self.image = self.animacao.pega_sprite_atual()
         self.rect = self.image.get_rect()
-        self.rect.x = tam_x
-        self.rect.y = tam_y
+        self.rect.x = self._pos_x
+        self.rect.y = self._pos_y
 
     def fazer_animacao(self, tipo):
         self.animacao.definir_estado(tipo)
 
-    def movimento(self):
+    def update(self):
          # Movimento Horizontal para o Urso
         if self._id_character == 2:
             self.rect.x += self.speed_x
@@ -209,22 +230,148 @@ class Inimigo(Personagens):
                     sprite = sprite_proj
         )
         return projetil
+    
 
-class Plataform(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h): #dimensões e posição
-        super().__init__()
-        self.image = pygame.Surface([w,h]) #criando a plataforma
-        self.image.fill([139, 69, 19])# as cores dela
-        #criando o hitbox dela:
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
 
-class Honey(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.Surface([20, 20])
-        self.image.fill([255, 215, 0])
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+pygame.init()
+
+# criando uma fonte para o placar
+game_font = pygame.font.Font(None, 36) # fonte padrão, tam.36
+display = pygame.display.set_mode([COMPRIMENTO_TELA, ALTURA_TELA])
+pygame.display.set_caption('Ursinho POO')
+clock = pygame.time.Clock()
+# --- Criação dos Objetos e Grupos ---
+Game = Pg.Jogo(1)
+sprite_inimigo_urso, sprite_poo, sprite_leitao, sprite_honey, sprite_boss, sprite_inimigo_abelha, sprite_mapa, sprite_projetil = Game.load_sprites_geral()
+ground_rect = pygame.Rect(0, 440, 840, 40)
+all_sprites = pygame.sprite.Group(); plataforms_group = pygame.sprite.Group()
+honey_group = pygame.sprite.Group(); enemies_group = pygame.sprite.Group()
+honey_score = 0; player = Player(
+    id_character=0,
+    name_character="Pooh",
+    sprite=sprite_poo,
+    pos_x=100,
+    pos_y=300,
+    tam_x=40,
+    tam_y=60,
+    speed_x=5,
+    speed_y=0
+); all_sprites.add(player)
+
+# Cria o urso
+enemy1 = Inimigo(
+        id_character= 3,
+        name_character= "Urso",
+        sprite = sprite_inimigo_urso,
+        pos_x= 300,
+        pos_y= 390,
+        vida=10, #adaptar posteriormente
+        tam_x=35,
+        tam_y= 50)
+all_sprites.add(enemy1); enemies_group.add(enemy1)
+
+# Cria a abelha
+bee1 = Inimigo(
+        id_character= 2,
+        name_character= "Abelha",
+        sprite = sprite_inimigo_abelha,
+        pos_x= 300,
+        pos_y= 390,
+        vida=10, #adaptar posteriormente
+        tam_x=30,
+        tam_y= 25)
+all_sprites.add(bee1); enemies_group.add(bee1)
+
+leitao = Npc(
+        id_character= 2,
+        name_character= "Leitao",
+        sprite = sprite_leitao,
+        pos_x= 50,    #definir quando o mapa ficar pronto
+        pos_y= 100,    #definir quando o mapa ficar pronto
+        vida = 9999,    #npc não morre
+        tam_x= 30,    #definir conforme a sprite
+        tam_y= 20,    #definir conforme a sprite
+    )
+all_sprites.add(leitao)
+
+for data in [[200, 350, 150, 20], [450, 280, 200, 20], [150, 180, 100, 20]]:
+    plat = Plataform(*data); all_sprites.add(plat); plataforms_group.add(plat)
+total_honey = len([[250, 325], [500, 255], [180, 155]])
+for data in [[250, 325], [500, 255], [180, 155]]:
+    honey = Honey(*data); all_sprites.add(honey); honey_group.add(honey)
+
+
+# Criando as plataformas:
+plat1 = Plataform(x=200, y=350, w=150, h=20)
+plat2 = Plataform(x=450, y=280, w=200, h=20)
+plat3 = Plataform(x=150, y=180, w=100, h=20)
+
+# Adicionando as plataformas aos grupos
+all_sprites.add(plat1, plat2, plat3)
+plataforms_group.add(plat1, plat2, plat3)
+
+#criando alguns potes de mel
+honey1 = Honey(250, 325, sprite_honey) # Em cima da plat1
+honey2 = Honey(500, 255, sprite_honey) # Em cima da plat2
+honey3 = Honey(180, 155, sprite_honey) # Em cima da plat3
+
+all_sprites.add(honey1, honey2, honey3)
+honey_group.add(honey1, honey2, honey3)
+
+honey_score = 0 # variável para contar o mel
+
+# --- Loop Principal ---
+gameLoop = True
+if __name__ == '__main__':
+    while gameLoop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameLoop = False
+        
+        # --- Lógica ---
+        # o .update() chama a função update de TODOS os sprites no grupo
+        all_sprites.update(plataforms_group)
+
+        honey_collected = pygame.sprite.spritecollide(player, honey_group, True)# O 'True' no final faz com que o mel atingido seja removido de todos os grupos
+        if honey_collected:
+            print("Mel coletado!")# No futuro, aqui aumentaremos um placar
+            honey_score += 1 #incrementaçao do mel
+        if not player.invincible:
+             enemy_hits = pygame.sprite.spritecollide(player, enemies_group, False)# verifica a colisão do player com o inimigo, o False serve para que o inimigo nao suma depois
+             if enemy_hits:
+                player.invincible = True
+                player.hurt_time = pygame.time.get_ticks()
+                player.vida -= 1
+                print(f'Atingido! vidas restanntes:{player.vida}')
+                enemy = enemy_hits[0]
+                if player.rect.centerx < enemy.rect.centerx: player.rect.x -= player.knockback_strength
+                else: player.rect.x += player.knockback_strength
+                player._speed_y = -8 # Impulso para cima
+
+        # verificação de GAME OVERdd
+        if player.vida <= 0:
+            print('GAME OVER!')
+            gameLoop = False # Jogo encerra
+
+
+        # --- Desenho ---
+        display.fill((93, 226, 231))
+        pygame.draw.rect(display, (34, 139, 34), ground_rect) # Desenha o chão verde
+        # o .draw() desenha TODOS os sprites do grupo na tela
+        all_sprites.draw(display)
+        
+        health_text = game_font.render(f'Vida:{player.vida}', True, (255,255,255))
+        display.blit(health_text, (10,10)) # posicao no canto superior esquerdo
+
+        honey_text = game_font.render(f'Mel: {honey_score}', True,(255,255,255))
+        display.blit(honey_text, (10,40))
+
+        if player.verifica_colisao(leitao):
+            leitao.comentario(display)
+
+
+
+        pygame.display.update()
+        clock.tick(60)
+
+pygame.quit()
