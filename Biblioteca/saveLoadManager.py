b2581cd022c9f3c96f62c89147aa29869c048e51
Lucas Leite <lucasleitlim@gmail.com>
2025-06-15 12:19:53 -0400

    alterando novamente a função saveload, ajustando a mesma ao codigo atualizado.

--- Biblioteca/saveLoadManager.py
+++ Biblioteca/saveLoadManager.py
@@ -3,35 +3,53 @@ import os
 
 class gerenciaSave:
     def __init__(self, arquivo_save='savegame.json'):
-        self.arquivo_save = arquivo_save
+        self.arquivo_save = arquivo_save #construtor
 
-    def salvar_jogo(self, player, plataforma, versao=1):
+    #função para salvar
+    def salvar_jogo(self, player,honey_score,game_state, versao=1):
         try:
+
+            #Escolhe as cenas para salvar e salva
+            cenas_para_salvar = {'level_1', 'boss_level'}
+            if game_state in cenas_para_salvar:
+                'current_scene' = game_state
+            # else:
+            #     'current_scene' = None
+
+            #seta os dados a serem salvos
             data = {
-                'id_G': player._id_game,
-                'amount_honey_coletada': player._amount_honey_coletada,
-                'name_C': player._name_character,
+                # Dados do player
                 'id_C': player._id_character,
-                'sprite_path_player': 'Assets/Poo/Idle',
+                'name_C': player._name_character,
+                'sprite_path_player': player.rect,
                 'position_x': player._pos_x,
                 'position_y': player._pos_y,
-                'life': player._vida,
                 'size_x': player._tam_x,
                 'size_y': player._tam_y,
                 'spd_x': player._speed_x,
                 'spd_y': player._speed_y,
-                'current_scene': plataforma.name_background,
-                'current_scene_id': plataforma.id_background,
-                'amount_honey_map': plataforma.amount_honey,
+
+                # Salva quantidade de méis coletados
+                'amount_honey_coletada': honey_score,
+
+                # Salva a vida atual do player
+                'Health': player.vida,
+
+                # Versão do jogo
                 'versao': versao
-                }
+            }
+
+            #escrita no arquivo
             with open(self.arquivo_save, 'w') as arquivo:
                 json.dump(data, arquivo)
             print("Jogo salvo com êxito!")
+
+        #tratamento de exceção
         except Exception as e:
             print(f"Erro ao salvar o jogo: {e}")
 
-    def carregar_jogo(self, PlayerClass, PlataformaClass, sprite_poo, sprite_mapa):
+    #função que carrega o jogo
+    def carregar_jogo(self, PlayerClass, honey_score, game_state):
         if not os.path.exists(self.arquivo_save):
             print("Arquivo de save não encontrado.")
             return None, None
@@ -45,38 +63,30 @@ class gerenciaSave:
                 return None, None
 
             player = PlayerClass(
-                id_game=data['id_G'],
-                name_character=data['name_C'],
                 id_character=data['id_C'],
-                sprite=sprite_poo,
+                name_character=data['name_C'],
+                sprite=data['sprite_path_player'],
                 pos_x=data['position_x'],
                 pos_y=data['position_y'],
-                vida=data['life'],
                 tam_x=data['size_x'],
                 tam_y=data['size_y'],
                 speed_x=data['spd_x'],
                 speed_y=data['spd_y'],
-                amount_honey_coletada=data['amount_honey_coletada']
             )
 
+            #carrega o mapa que parou
+            cenas_para_carregar = {'level_1', 'boss_level'}
+            if game_state in cenas_para_carregar:
+                game_state = data['current_scene'],
+            
+            #carrega a quantidade de mel
+            honey_score = data['amount_honey_coletada'],
 
-            plataforma = PlataformaClass(
-                id_game=1,
-                amount_honey=data['amount_honey_map'],
-                status=1,
-                id_background=data['current_scene_id'],
-                id_plataforma=1,
-                name_background=data['current_scene'],
-                sprite_background=sprite_mapa,
-                posx_plataforma=400,
-                posy_plataforma=300,
-                largura=0,
-                altura=0
-            )
-
-
+            #carrega a quantidade de vida
+            player.vida = data['Health'],
+            
             print("Jogo carregado com êxito!")
-            return player, plataforma
+            return player, honey_score
 
         except Exception as e:
             print(f"Erro ao carregar o jogo: {e}")
